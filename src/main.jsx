f638c58eca8cefda6096e2f55fa1d793071c8e2e
shreyasj1508 <shreyasjaiswal1515@gmail.com>
2025-07-20 20:26:59 +0530

    made /Login UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import store from "./store";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        {" "}
+        {}
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
