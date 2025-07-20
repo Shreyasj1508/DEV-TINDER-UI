20ec0f128f803968f89a76d0217077f7342b12fc
shreyasj1508 <shreyasjaiswal1515@gmail.com>
2025-07-20 14:39:05 +0530

    some setup changes + navbar using DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-// tailwind.config.js
-
-import daisyui from "daisyui";
-
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-};
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [daisyui],
+  };
+  
