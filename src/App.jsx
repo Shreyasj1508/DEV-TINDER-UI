49985cf53e973217c624220651619f47a5a1ff89
shreyasj1508 <shreyasjaiswal1515@gmail.com>
2025-07-21 15:10:56 +0530

    fix few bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Body from "./Components/Body";
 import Login from "./Components/Login";
+import Profile from "./Components/Profile";
 import Feed from "./Components/Feed";
-import { useDispatch } from "react-redux";
-import { addUser } from "./utils/userSlice";
-import { useEffect } from "react";
-import { BASE_URL } from "./utils/constants";
-import axios from "axios";
+import "./index.css";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Body />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom"; 
 import store from "./store";
-import App from "./App.jsx";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        {" "}
-    
         <App />
       </BrowserRouter>
     </Provider>
-  </StrictMode>
+  </React.StrictMode>
 );
