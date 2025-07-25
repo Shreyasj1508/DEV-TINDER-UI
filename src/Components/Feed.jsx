4ca8825a4285e4678cf7bb7f8744f786eb7a5342
shreyasj1508 <shreyasjaiswal1515@gmail.com>
2025-07-25 18:51:08 +0530

    feed is now showing

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -8,36 +8,36 @@ import UserCard from "./userCard";
 const Feed = () => {
   const dispatch = useDispatch();
   const feed = useSelector((store) => store.feed);
-  console.log(feed);
+
   const getFeed = async () => {
-    if (feed) return;
+    if (feed && feed.length > 0) return;
     try {
-      const feed = await axios.get(BASE_URL + "/user/feed", {
+      const response = await axios.get(BASE_URL + "/user/feed", {
         withCredentials: true,
       });
-      dispatch(addFeed(feed.data));
-      // console.log(feed);
+      dispatch(addFeed(response.data.data)); // ✅ Fix here
     } catch (err) {
       console.log(err);
     }
   };
+
   useEffect(() => {
     getFeed();
-  });
+  }, []); // ✅ only run once
 
-  if (!feed) return;
+  if (!Array.isArray(feed)) return null;
 
   if (feed.length <= 0)
     return (
-      <h1 className=" flex justify-center m-52 text-3xl">No more users!!!!</h1>
+      <h1 className="flex justify-center m-52 text-3xl">No more users!!!!</h1>
     );
+
   return (
-    feed && (
-      <div className="flex flex-col items-center gap-4 my-5">
-        {feed && feed.map((user) => <UserCard key={user._id} user={user} />)}
-        {/* <UserCard user={feed[0]} /> */}
-      </div>
-    )
+    <div className="flex flex-col items-center gap-4 my-5">
+      {feed.map((user) => (
+        <UserCard key={user._id} user={user} />
+      ))}
+    </div>
   );
 };
 
