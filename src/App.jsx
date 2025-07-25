de757bd21347072daf9c18c8a4826676a8c0e06f
shreyasj1508 <shreyasjaiswal1515@gmail.com>
2025-07-25 13:55:13 +0530

    code complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./Components/Body";
 import Login from "./Components/Login";
 import Profile from "./Components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 import Feed from "./Components/Feed";
-import "./index.css";
-import Premium from "./Components/Premium";
-import Requests from "./Components/Requests";
 import Connections from "./Components/Connections";
+import Requests from "./Components/Requests";
+import Premium from "./Components/Premium";
+import Chat from "./Components/Chat";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Body />} />
-      <Route path="/" element={<Feed />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/premium" element={<Premium />} />
-      <Route path="/requests" element={<Requests />} />
-      <Route path="/connections" element={<Connections />} />
-    </Routes>
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+              <Route path="/premium" element={<Premium />} />
+              <Route path="/chat/:targetUserId" element={<Chat />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 }
 
